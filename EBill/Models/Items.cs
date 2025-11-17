d69e1fce3f04b58ec8e823237738c2c58f49b9bd
yashtalaviya5 <yash.070752@gmail.com>
2025-11-17 16:29:16 +0530

    Initial commit

---
+++ EBill/Models/Items.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EBill.Models
+{
+    public class Items
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public decimal Quantity { get; set; }
+        public int ItemIndex { get; set; }
+
+    }
+}
---
+++ EBill/Repository/IData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EBill.Models;
+using System.Data.SqlClient;
+
+namespace EBill.Repository
+{
+    internal interface IData
+    {
+        void SaveBillDetails(BillDetail details);
+        void SaveBillItems(List<Items> items, SqlConnection con,int id);
+        List<BillDetail> GetAllDetail();
+        BillDetail GetDetail(int id);
+    }
+}
